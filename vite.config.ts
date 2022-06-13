fd7a5b2b12391b919ae1e7099e14cb503af97d31
@cmmdmx <me@cmmdmx.de>
2022-06-13 12:21:41 +0200

    test better cjs support

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         lib:         {
             entry:    path.resolve(__dirname, "src/index.ts"),
             name:     "palstek",
+            formats:  ["es", "cjs", "umd"],
             fileName: format => `palstek.${format}.js`
         },
         rollupOptions: {
@@ -27,13 +28,12 @@ export default defineConfig({
                     react: "React"
                 },
                 sourcemap:               true,
-                sourcemapExcludeSources: true,
-                compact:                 true
+                sourcemapExcludeSources: true
             }
         },
         commonjsOptions: {
             transformMixedEsModules: true,
-            sourceMap:               false
+            sourceMap:               true
         }
     }
 });
