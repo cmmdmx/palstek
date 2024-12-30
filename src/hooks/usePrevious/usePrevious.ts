4c1ecf4267c9ee389a39835023c2b4c13508e02b
@cmmdmx <me@cmmdmx.de>
2024-12-30 17:15:39 +0100

    package maintenance and fix export mjs format

--- src/hooks/usePrevious/usePrevious.ts
+++ src/hooks/usePrevious/usePrevious.ts
@@ -6,7 +6,7 @@ import { useEffect, useRef } from "react";
  * @returns {T | undefined}
  */
 export const usePrevious = <T>(value: T): T | undefined => {
-    const ref = useRef<T>();
+    const ref = useRef<T>(undefined);
 
     useEffect(() => {
         ref.current = value;
